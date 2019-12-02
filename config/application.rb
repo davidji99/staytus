e0315f333ecff2cc28121c3d35221a2ee3bfb2ec
DJ <davidji99@users.noreply.github.com>
2019-12-02 13:37:25 +0900

    update application.rb [#1]

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,27 @@ Bundler.require(*Rails.groups)
 
 module Staytus
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
+    # Disable this to make comments with active admin work
+    config.active_record.belongs_to_required_by_default = false
+
+    # Load anything before the application starts
+    config.before_initialize do
+      # leaving empty for now
+    end
+
+    # Enable escaping HTML in JSON.
+    config.active_support.escape_html_entities_in_json = true
+
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
+
+    # make sure of structure.sql vs schema.rb
+    config.active_record.schema_format = :sql
+
     config.eager_load_paths += %W(#{config.root}/lib)
     config.generators do |g|
       g.orm             :active_record
